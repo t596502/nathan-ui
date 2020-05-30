32f0f79d6af500a93e284dbc8f71a89ae9f12d95
Nathan Tai <nathantai@outlook.com>
2020-05-30 22:58:23 +0800

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Button from './components/Button'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button btnType='danger'>222</Button>
+      <Button btnType='primary' size='lg'>primary</Button>
+      <Button btnType='primary' size='sm'>primary</Button>
+      <Button btnType='primary' disabled>disabled</Button>
+      <Button btnType='link' href='http://www.baidu.com' disabled>link-disabled</Button>
+      <Button btnType='link' href='http://www.baidu.com'>link</Button>
     </div>
   );
 }
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,50 @@
+import React ,{FC}from 'react'
+import classNames from 'classnames'
+export type ButtonSize = 'lg' | 'sm'
+export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
+
+interface BaseButtonProps {
+    className?:string;
+    /**设置 Button 的禁用 */
+    disabled?:boolean;
+    /**设置 Button 的尺寸 */
+    size?:ButtonSize;
+    /**设置 Button 的类型 */
+    btnType?:ButtonType;
+    children:React.ReactNode;
+    href?:string
+}
+
+
+const Button:FC<BaseButtonProps> = (props)=> {
+    const {className,disabled,size,btnType,children,href,...restProps} = props;
+
+    const classes= classNames('btn',className,{
+        [`btn-${size}`]:size,
+        [`btn-${btnType}`]:btnType,
+        'disabled':btnType === 'link' && disabled
+    });
+        if(btnType === 'link' && href){
+            return (
+                <a className={classes}
+                   href={href}
+                   {...restProps}
+            >{children}</a>
+            )
+        }else{
+            return (
+                <button disabled={disabled}
+                        className={classes}
+                        {...restProps}
+                >
+                    {children}
+                </button>
+            )
+        }
+}
+
+Button.defaultProps={
+    btnType:'default',
+    disabled:false
+}
+export default Button
