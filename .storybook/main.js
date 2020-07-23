b767b1a49dd58c50bad6e669ac3d99d375bd4a01
Nathan Tai <nathantai@outlook.com>
2020-07-23 10:28:53 +0800

    配置storybook.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,31 @@
+const path = require('path');
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
   ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules = config.module.rules.filter(rule => {
+      if(rule.test) {
+        return !rule.test.test(".scss")
+      }
+    });
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader',{
+        loader:'sass-loader',
+      }],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    // Return the altered config
+    return config;
+  },
 };
