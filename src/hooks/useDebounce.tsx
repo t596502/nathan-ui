1b06b4f2cabd3109005384cbfdc262ef8621bbab
Nathan Tai <nathantai@outlook.com>
2020-08-09 22:04:43 +0800

    1.add useClickOutside 2.add useDebounce

---
+++ src/hooks/useDebounce.tsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+
+function useDebounce(value:any,delay:number=300) {
+    const [debouncedValue, setDebouncedValue] = useState(value)
+    useEffect(()=>{
+        const handler = window.setTimeout(()=>{
+            setDebouncedValue(value)
+        },delay)
+        return ()=>{
+            clearTimeout(handler)
+        }
+    },[value,delay])
+    return debouncedValue
+}
+
+
+export default useDebounce
